0a7244143d16a3989e2f212501aab16b45469f1e
Rexadell <reksaparamaaditya@gmail.com>
2025-04-21 14:33:00 +0800

    App-Prototype

---
+++ script.js
@@ -0,0 +1,35 @@
+const addPlayer = document.getElementById('add-player-btn');
+const playerInput = document.getElementById('player-input');
+const playersList = document.getElementById('players-list');
+const reset = document.getElementById('reset');
+
+// Player Input
+addPlayer.addEventListener('click', () => {
+  const player = document.createElement('li');
+  const countAdd = document.createElement('button');
+  const countMin = document.createElement('button');
+  const total = document.createElement('p');
+  player.textContent = playerInput.value;
+  playersList.appendChild(player);
+  playerInput.value = '';
+  countAdd.textContent = '+100';
+  player.appendChild(countAdd);
+  countMin.textContent = '-50';
+  player.appendChild(countMin);
+  total.textContent = '0';
+  player.appendChild(total);
+  if (countAdd.addEventListener && countMin.addEventListener) {
+    countAdd.addEventListener('click', () => {
+      total.textContent = parseInt(total.textContent) + 100;
+    });
+    countMin.addEventListener('click', () => {
+      total.textContent = parseInt(total.textContent) - 50;
+    });
+  }
+});
+
+if (reset.addEventListener) {
+  reset.addEventListener('click', () => {
+    playersList.innerHTML = '';
+  });
+}
